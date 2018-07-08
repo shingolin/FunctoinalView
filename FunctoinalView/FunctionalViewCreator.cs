84cb3ee46f023a392a49122700ea6b0cf1676a70
shingolin <shingolin@gmail.com>
2018-07-09 01:31:34 +0800

    加入專案檔案。

---
+++ FunctoinalView/FunctionalViewCreator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Dapper;
+using FunctoinalView;
+
+namespace Autifac_Demo
+{
+    public class FunctionalViewCreator
+    {
+        IDbConnection _dbConnection;
+        int _ViewId;
+        int _FunctionId;
+        public string command = "";
+        public List<FunctionalColumnProperty> functionalColumns = new List<FunctionalColumnProperty>();
+        public FunctionalViewCreator(IDbConnection dbConnection, int ViewId, int FunctionId)
+        {
+            _dbConnection = dbConnection;
+            _ViewId = ViewId;
+            _FunctionId = FunctionId;
+        }
+        public DataTable GetFunctionalView(DataTable data)
+        {
+            foreach (var column in functionalColumns)
+            {
+                DataColumn dc = new DataColumn(column.ColumnName);
+                data.Columns.Add(dc);
+            }
+
+            foreach (DataRow r in data.Rows)
+            {
+                foreach (var column in functionalColumns)
+                {
+                    NCalc.Expression expression = new NCalc.Expression(column.Formula);
+                    foreach (DataColumn dc in data.Columns)
+                    {
+                        expression.Parameters.Add(dc.ColumnName, r[dc.ColumnName]);
+                    }
+                    r[column.ColumnName] = expression.Evaluate();
+                }
+            }
+            return data;
+        }
+        public DataTable GetView(Dictionary<string, object> parameters)
+        {
+            var reader = _dbConnection.ExecuteReader(command);
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            return dataTable;
+        }
+    }
+}
+
+namespace FunctoinalView
+{
+    public class FunctionalColumnProperty
+    {
+        public string ColumnName { get; internal set; }
+        public string Formula { get; internal set; }
+    }
+}
---
+++ FunctoinalView/Program.cs
@@ -0,0 +1,33 @@
+﻿using Autifac_Demo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctoinalView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "192.168.1.24";
+            connectionStringBuilder.InitialCatalog = "corsairhr";
+            connectionStringBuilder.UserID = "jb";
+            connectionStringBuilder.Password = "JB8421";
+            SqlConnection connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+            FunctionalViewCreator creator = new FunctionalViewCreator(connection, 1, 1);
+            creator.command = "select * from abs where nobr='960002' and bdate between '20171201' and '20171231'";
+            creator.functionalColumns = new List<FunctionalColumnProperty>();
+            creator.functionalColumns.Add(new FunctionalColumnProperty
+            {
+                ColumnName = "Test1",
+                Formula = "TOL_HOURS*30"
+            });
+            var result = creator.GetView(new System.Collections.Generic.Dictionary<string, object>());
+            var result1 = creator.GetFunctionalView(result);
+        }
+    }
+}
